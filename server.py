ea10b97ed2ee4040844b52178685083807c52db0
MBAir <283878@student.pwr.edu.pl>
2026-05-29 08:37:26 +0200

    Stripped down launch script

--- server.py
+++ server.py
@@ -1,39 +1,6 @@
-from dotenv import load_dotenv
-import json
-import keyExchange
-import os
-import psycopg2
-import socket
-import threading
-from user import User
+from srvClass import Server
 
-def connectionHandler(conn, addr):
-  user = User(conn,addr, dbConnection)
-  conn.send('Thank you for connecting!'.encode())
-  conn.send(str(user.getSrvPubKey()).encode())
+server = Server()
 
-load_dotenv()
-
-dbUrl = os.getenv('DATABASE_URL')
-port = int(os.getenv('SRV_PORT'))
-maxClientCount = int(os.getenv('SRV_MAX_CONN'))
-
-dbConnection = psycopg2.connect(dbUrl)
-
-print(f"Port: {port}\nMax klientów: {maxClientCount}")
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket successfully created")
-
-server.bind(('0.0.0.0', port))
-print("Socket bound to port %s" %(port))
-
-server.listen(maxClientCount)
-print("socket is listening")
-
-while True: 
-
-  client, addr = server.accept()     
-  print('Got connection from', addr)
-
-  threading.Thread(target=connectionHandler,args=(client,addr)).start()
+server.openConnection()
+server.listen()
